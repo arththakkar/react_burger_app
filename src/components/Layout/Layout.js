c4b5181efdd885789e4a5bfedcfb227ece3443b1
Arth Thakkar <arththakkar@gmail.com>
2020-05-18 01:03:14 +0530

    Making design more responsive and adding hamburger icon for mobile device menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
-export default layout;
+class Layout extends Component{
+    state = {
+        showSideDrawer: true
+    };
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    toggleSideDrawerHandler = () => {
+        this.setState({showSideDrawer: true});
+    }
+    render () {
+        return (
+            <Aux>
+                <Toolbar open={this.toggleSideDrawerHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+export default Layout;
